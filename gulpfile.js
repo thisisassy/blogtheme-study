0bf02d10649f9de8dbe2f74419b42bd8b3fdb92c
Haruka Ashiuchi <chopperman1224@gmail.com>
2020-03-17 12:56:18 +0900

    ブログの全体構造をつくる

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,27 @@
-const gulp = require("gulp");
-const less = require("gulp-less");
+const gulp = require('gulp');
+const less = require('gulp-less');
+const bs = require('browser-sync');
 
-gulp.task("default",function(){
+gulp.task('serve',function(){
+    bs.init({
+        server: './dest',
+        open: false,
+        port: 3030,
+        ui: {
+            port: 3031,
+        },
+    });
+
+    gulp.watch('src/**/*.less',gulp.parallel('less'));
+});
+
+gulp.task('less', function(){
     return gulp
-    .src('src/*.less')
+    .src(['src/*.less'],['!src/_*.less'])
     .pipe(less({
         outputStyle: 'expanded'
     }))
-    .pipe(gulp.dest('dest'));
+    .pipe(gulp.dest('./dest'));
 });
 
-gulp.task('watch',function(){
-    return gulp.watch('./*.less',['less']);
-});
+gulp.task('default',gulp.parallel('serve'));
